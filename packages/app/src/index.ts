653c249c9900f489dedcbe14f89177636b9af809
dePage <chris.depage@gmail.com>
2022-04-30 15:09:44 -0400

    Simplify internal app dependencies

--- packages/app/src/index.ts
+++ packages/app/src/index.ts
@@ -1,5 +1,1 @@
-export * from '@zerodep/can';
-export * from '@zerodep/errors';
-export * from '@zerodep/guard';
-export * from '@zerodep/is';
-export * from '@zerodep/to';
+export * from '@zerodep/utils';
